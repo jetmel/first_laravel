9d191e4d0a7cc3c1088f175a96e834da84f2562e
meijun <85771158@qq.com>
2017-06-10 18:00:46 +0800

    Finish static pages

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Sample App - About</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">I Love You, piggy!</div>
+                <div class="title">关于页</div>
             </div>
         </div>
     </body>
